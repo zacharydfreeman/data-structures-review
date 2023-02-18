dbcdb4264e80569146e7bcb78f0cf6799a8be4ed
Zachary Freean <zdf2424@gmail.com>
2023-02-18 04:50:51 -0600

    initial question list

---
+++ Binary Trees/bfs_dfs.js
@@ -0,0 +1,26 @@
+/**
+ * Write a function, breadthFirstValues, that takes in the root of a binary tree.
+ * The function should return an array containing all values of the tree in breadth-first order.
+ *
+ *
+ */
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+// Approach:
+// Time & Space Complexity:
+const dfs = (root) => {
+  // TO DO
+};
+
+// Approach:
+// Time & Space Complexity:
+const bfs = (root) => {
+  // TO DO
+};
---
+++ Linked_Lists/reverse-LL.js
@@ -0,0 +1,18 @@
+/**
+ * Write a function, reverseList, that takes in the head of a linked list as an argument.
+ * The function should reverse the order of the nodes in the linked list in-place and return the new head of the reversed linked list.
+ *
+ */
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+// Approach:
+// Time & Space Complexity:
+const reverseList = (head) => {
+  // TO DO
+};
