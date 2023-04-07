08d5e25dfdd7fa297d9a46830dfe698e02bdb366
zacharydfreeman <zdf2424@gmail.com>
2023-04-07 12:59:00 -0500

    merge k sorted lists

---
+++ Heaps/merge-ksorted-lists.js
@@ -0,0 +1,20 @@
+/* 
+https://leetcode.com/problems/merge-k-sorted-lists/description/
+
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+Merge all the linked-lists into one sorted linked-list and return it.
+
+*/
+class ListNode {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+// Approach:
+// Time & Space Complexity:
+const mergeKLists = (lists) => {};
+
+module.exports = { mergeKLists };
